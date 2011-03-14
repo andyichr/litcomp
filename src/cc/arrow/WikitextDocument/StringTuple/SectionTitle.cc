a94b5ef6357ca92af3885807faee8f527a55ed28
Andy Christianson <git@andyic.org>
2011-03-13 20:17:17 -0400

    added initial tinyxml behavior

--- src/cc/arrow/WikitextDocument/StringTuple/SectionTitle.cc
+++ src/cc/arrow/WikitextDocument/StringTuple/SectionTitle.cc
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <tinyxml/tinyxml.h>
 
 #include "SectionTitle.h"
 
 int main(void) {
+	TiXmlDocument doc("/dev/stdin");
+	bool loadOK = doc.LoadFile();
+
+	if (loadOK) {
+		std::cout << "loaded XML OK\n";
+	}
+	else {
+		std::cout << "failed loading XML\n";
+	}
+
 	std::cout << "Hello World!\n";
 	return 0;
 }
