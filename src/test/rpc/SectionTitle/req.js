27dd7ff758eb98376f3870271932cb17570b9141
Andy Christianson <git@andyic.org>
2011-03-24 21:07:23 -0400

    initial commit

---
+++ src/test/rpc/SectionTitle/req.js
@@ -0,0 +1,13 @@
+{
+	"take": {
+		"take": {
+			"take": {
+				"this": { "title": "Index", "sha1": "" },
+				"from": "ArticleVersion",
+				"to": "Wikitext"
+			}
+		}
+		"to": "StringTuple",
+		"using": "SectionTitle"
+	}
+}
