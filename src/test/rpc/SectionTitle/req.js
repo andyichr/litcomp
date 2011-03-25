279919baaa75676659c42279b953247612e0c966
Andy Christianson <git@andyic.org>
2011-03-25 01:19:50 -0400

    added realistic test data

--- src/test/rpc/SectionTitle/req.js
+++ src/test/rpc/SectionTitle/req.js
@@ -1,12 +1,12 @@
 {
 	"take": {
 		"take": {
-			"take": {
-				"this": { "title": "Index", "sha1": "" },
-				"from": "ArticleVersion",
-				"to": "Wikitext"
-			}
-		}
+			"this": { "title": "Index", "hash": "12d37f9f294373843a56d27200843cf0f8b4c984" },
+			"from": "ArticleVersion",
+			"to": "WikitextDocument",
+			"using": "Source"
+		},
+		"from": "WikitextDocument",
 		"to": "StringTuple",
 		"using": "SectionTitle"
 	}
