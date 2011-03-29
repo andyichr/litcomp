b077b93ddcabbb25b687ac9ad292117cdedf372b
Andy Christianson <git@andyic.org>
2011-03-28 21:22:09 -0400

    added IO redirection so that stderr of subprocess is captured

--- src/test/rpc/rpc.sh
+++ src/test/rpc/rpc.sh
@@ -14,5 +14,5 @@ find "$TEST_DIR"/rpc/* -type d | while read RPC_TEST; do
 	echo "Invoking RPC test: $(basename "$RPC_TEST")"
 	test -f "$RPC_TEST"/req.js || touch "$RPC_TEST"/req.js
 	test -f "$RPC_TEST"/res.js || touch "$RPC_TEST"/res.js
-	diff -u "$RPC_TEST"/res.js <("$SRC_DIR"/pl/rpc.pl < "$RPC_TEST"/req.js) && echo "TEST PASS" || echo "TEST FAIL"
+	diff -u "$RPC_TEST"/res.js <("$SRC_DIR"/pl/rpc.pl < "$RPC_TEST"/req.js 2>&1) && echo "TEST PASS" || echo "TEST FAIL"
 done
