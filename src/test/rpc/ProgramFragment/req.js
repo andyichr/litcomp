4790f14588a9d54ee6965a79ff4ef821a4bc02d9
Andy Christianson <git@andyic.org>
2011-03-28 21:19:15 -0400

    initial commit

---
+++ src/test/rpc/ProgramFragment/req.js
@@ -0,0 +1,13 @@
+{
+	"take": {
+		"take": {
+			"this": { "title": "Litcomp", "hash": "bda4459a70dd3e49dbe4a80a096b3d611e18e0bf" },
+			"from": "ArticleVersion",
+			"to": "WikitextDocument",
+			"using": "Source"
+		},
+		"from": "WikitextDocument",
+		"to": "ProgramFragmentTuple",
+		"using": "ProgramFragment"
+	}
+}
