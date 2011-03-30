8de6a002778678556969644922ea71b350bdffdd
Andy Christianson <git@andyic.org>
2011-03-29 20:34:57 -0400

    initial commit

---
+++ src/test/rpc/RunIndex/req.js
@@ -0,0 +1,21 @@
+{
+	"take": {
+		"take": {
+			"take": {
+				"this": {
+					"title": "Litcomp",
+					"hash": "bda4459a70dd3e49dbe4a80a096b3d611e18e0bf"
+				},
+				"from": "ArticleVersion",
+				"to": "WikitextDocument",
+				"using": "Source"
+			},
+			"from": "WikitextDocument",
+			"to": "ProgramFragmentTuple",
+			"using": "ProgramFragment"
+		},
+		"from": "ProgramFragmentTuple",
+		"to": "NatTuple",
+		"using": "RunIndex"
+	}
+}
