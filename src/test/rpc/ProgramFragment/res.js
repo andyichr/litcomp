cd9c5fdb86737dca90396ba9b4336c74bc0ec463
Andy Christianson <git@andyic.org>
2011-03-29 20:28:17 -0400

    corrected handling of newline characters in JSON output

--- src/test/rpc/ProgramFragment/res.js
+++ src/test/rpc/ProgramFragment/res.js
@@ -1,5 +1,1 @@
-[{"section_index": 7, "pre_index": 1, "lang": "bash", "source": "
-test
-test
-test
-"}, {"section_index": 9, "pre_index": 2, "lang": "perl", "source": "print \"Hello World!\";"}]
+[{"section_index": 7, "pre_index": 1, "lang": "bash", "source": "\ntest\ntest\ntest\n"}, {"section_index": 9, "pre_index": 2, "lang": "perl", "source": "print \"Hello World!\";"}]
