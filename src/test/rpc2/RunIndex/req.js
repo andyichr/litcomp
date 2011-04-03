40ae151f18dc913f42ad434068bfebd4fe7fd3e4
Andy Christianson <git@andyic.org>
2011-04-03 16:30:11 -0400

    ported RunIndex test from rpc interface to new rpc2 interface

---
+++ src/test/rpc2/RunIndex/req.js
@@ -0,0 +1,4 @@
+[ "map", "RunIndex : _ -> NatTuple",
+	[ "map", "ProgramFragment : _ -> ProgramFragmentTuple",
+		[ "map", "Source : ArticleVersion -> WikitextDocument",
+			{ "title": "Litcomp", "hash": "bda4459a70dd3e49dbe4a80a096b3d611e18e0bf" } ] ] ]
