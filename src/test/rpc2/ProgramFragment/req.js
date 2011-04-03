83ee9b5443d572bfbe37bce6ae791d27979ba3cc
Andy Christianson <git@andyic.org>
2011-04-03 16:21:22 -0400

    ported ProgramFragment test from rpc interface to new rpc2 interface

---
+++ src/test/rpc2/ProgramFragment/req.js
@@ -0,0 +1,5 @@
+[ "map", "ProgramFragment : _ -> ProgramFragmentTuple",
+	[ "map", "Source : ArticleVersion -> WikitextDocument",
+		{ "title": "Litcomp", "hash": "bda4459a70dd3e49dbe4a80a096b3d611e18e0bf" } ] ]
+
+
