9433995e3d8d1a5bbcbb27482fb4288bc83fa14a
Andy Christianson <git@andyic.org>
2011-04-03 16:22:59 -0400

    cleaned up formatting

--- src/test/rpc2/ProgramFragment/req.js
+++ src/test/rpc2/ProgramFragment/req.js
@@ -1,5 +1,3 @@
 [ "map", "ProgramFragment : _ -> ProgramFragmentTuple",
 	[ "map", "Source : ArticleVersion -> WikitextDocument",
 		{ "title": "Litcomp", "hash": "bda4459a70dd3e49dbe4a80a096b3d611e18e0bf" } ] ]
-
-
