8716610752b2348c95c82b7a6f140f7ddb9ef476
Andy Christianson <andy@andyic.org>
2011-04-20 17:10:00 -0400

    added SectionIndex test case

---
+++ src/test/rpc2/SectionIndex/req.js
@@ -0,0 +1,1 @@
+[ "map", "SectionIndex : ProgramFragmentTuple -> HashTable", [{"section_index": 7, "pre_index": 1, "lang": "bash", "source": "\ntest\ntest\ntest\n"}, {"section_index": 9, "pre_index": 2, "lang": "perl", "source": "print \"Hello World!\";"}] ]
---
+++ src/test/rpc2/SectionIndex/res.js
@@ -0,0 +1,1 @@
+{"7":{"source":"\ntest\ntest\ntest\n","pre_index":1,"lang":"bash","section_index":7},"9":{"source":"print \"Hello World!\";","pre_index":2,"lang":"perl","section_index":9}}
